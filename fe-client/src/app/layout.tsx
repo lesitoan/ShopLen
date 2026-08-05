4cecb40c6d155529ddfb99bca87d70e8b4748f4a
lesitoan <lesitoan.media@gmail.com>
2026-08-05 22:41:44 +0700

    feat(client): vercel analytics package

--- fe-client/src/app/layout.tsx
+++ fe-client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Be_Vietnam_Pro } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 import NavigationReset from "@/components/common/NavigationReset";
@@ -28,6 +29,7 @@ export default function RootLayout({
           <NavigationReset />
           {children}
         </AppProviders>
+        <Analytics />
       </body>
     </html>
   );
